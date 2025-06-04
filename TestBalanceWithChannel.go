e0535c2b7ca1d1e24d78e657465ccad68de20c4b
Nguyen Ngoc Phuc <nnp.uet@gmail.com>
2025-06-04 10:45:13 +0700

    Improve goroutine cleanup

--- TestBalanceWithChannel.go
+++ TestBalanceWithChannel.go
@@ -12,31 +12,32 @@ func init() {
 	balance1 = 100
 }
 
-func deposit1(val int, wg sync.WaitGroup, ch chan bool) {
+func deposit1(val int, wg *sync.WaitGroup, ch chan bool) {
+	defer wg.Done()
 	ch <- true
 	fmt.Println("deposit before", balance1)
 	balance1 += val
 	fmt.Println("deposit after", balance1)
 	<-ch
 	time.Sleep(time.Millisecond * 10)
-	wg.Done()
 }
 
-func withdraw1(val int, wg sync.WaitGroup, ch chan bool) {
+func withdraw1(val int, wg *sync.WaitGroup, ch chan bool) {
+	defer wg.Done()
 	ch <- true
 	fmt.Println("withdraw before", balance1)
 	balance1 -= val
 	fmt.Println("withdraw after", balance1)
 	<-ch
-	wg.Done()
 }
 
 func main() {
 	ch := make(chan bool, 1)
 	var wg sync.WaitGroup
-	go deposit1(20, wg, ch)
-	go withdraw1(80, wg, ch)
-	go deposit1(40, wg, ch)
-	<-ch
-	fmt.Printf("Balance is: %d\n", balance)
+	wg.Add(3)
+	go deposit1(20, &wg, ch)
+	go withdraw1(80, &wg, ch)
+	go deposit1(40, &wg, ch)
+	wg.Wait()
+	fmt.Printf("Balance is: %d\n", balance1)
 }
